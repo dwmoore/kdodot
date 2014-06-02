69f36e0c5a148b413e5786fbe75581c89cd29ea0
David Moore <khundawg@gmail.com>
2014-06-02 11:32:22 -0400

    Clean up a few more files before merge

--- spec/controllers/todo_items_controller_spec.rb
+++ spec/controllers/todo_items_controller_spec.rb
@@ -2,14 +2,4 @@ require 'spec_helper'
 
 describe TodoItemsController do
 
-<<<<<<< HEAD
-=======
-  describe "GET 'index'" do
-    it "returns http success" do
-      get todo_list_todo_items_path
-      response.should be_success
-    end
-  end
-
->>>>>>> master
 end
