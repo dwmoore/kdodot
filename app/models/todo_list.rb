1116a335475a9e7589a970698efcec0be09fdc33
David Moore <khundawg@gmail.com>
2014-06-02 20:30:07 -0400

    Update show view and specs, Fix broken spec

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -4,4 +4,12 @@ class TodoList < ActiveRecord::Base
 	validates :title, presence: true, length: { minimum: 3 }
 	validates :description, presence: true, length: { minimum: 3 }
 
+
+	def has_complete_items?
+		todo_items.complete.size > 0
+	end
+
+	def has_incomplete_items?
+		todo_items.incomplete.size > 0
+	end
 end
--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -2,4 +2,33 @@ require 'spec_helper'
 
 describe TodoList do
 	it { should have_many(:todo_items) }
+
+	describe "#has_complete_items?" do
+		let(:todo_list) {TodoList.create(title: "Todo List", description: "This is a test list") }
+
+		it "returns true with completed todo list items" do
+			todo_list.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)
+			expect(todo_list.has_complete_items?).to be_true
+		end
+
+		it "returns false with no completed todo items" do
+			todo_list.todo_items.create(content: "Eggs")
+			expect(todo_list.has_complete_items?).to be_false
+		end
+	end
+
+	describe "#has_incomplete_items?" do
+		let(:todo_list) {TodoList.create(title: "Todo List", description: "This is a test list") }
+
+		it "returns true with incompleted todo list items" do
+			todo_list.todo_items.create(content: "Eggs")
+			expect(todo_list.has_incomplete_items?).to be_true
+		end
+
+		it "returns false with no incompleted todo items" do
+			todo_list.todo_items.create(content: "Eggs", completed_at: 1.minute.ago)
+			expect(todo_list.has_incomplete_items?).to be_false
+		end
+	end
+
 end
